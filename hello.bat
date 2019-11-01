2817961b176539cf6b6bb4213b362b7e2bf18640
Balasubrahmanyam <pbs@gmail.com>
2019-11-01 20:06:45 +0530

    added bat script to define and initialize variables in batch

---
+++ hello.bat
@@ -0,0 +1,16 @@
+@rem This is my first bat script!!!!!
+
+@echo off
+
+set name=TRaa
+set /A  age=30
+echo %name%
+echo %age%
+@echo var is = %1
+
+set /A sum=age + %1
+echo sum is : %sum%
+
+
+
+pause
